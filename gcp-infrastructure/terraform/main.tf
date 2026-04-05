2392737f2c7d766423f91ce308ef869ec4655c71
Pablodd <pablo.perez92@hotmail.com>
2026-04-05 09:24:46 +0000

    ID-1.1: Añadida configuración de red y firewall

--- gcp-infrastructure/terraform/main.tf
+++ gcp-infrastructure/terraform/main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "streaming_bucket" {
   name          = "tfg-stream-bucket-2026-tf" # Un nombre único
-  location      = "EU"
+  location      = "EUROPE-SOUTHWEST1"
   force_destroy = true
 
   cors {
---
+++ gcp-infrastructure/terraform/network.tf
@@ -0,0 +1,52 @@
+# La Red VPC principal
+resource "google_compute_network" "vpc_network" {
+  name                    = "red-stream-tfg-tf" # Le añado -tf para no chocar con la actual
+  auto_create_subnetworks = false
+}
+
+# La Subred de Madrid
+resource "google_compute_subnetwork" "subnet_madrid" {
+  name          = "subred-madrid-tf"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "europe-southwest1"
+  network       = google_compute_network.vpc_network.id
+}
+
+# Regla para RTMP (Ingesta de OBS)
+resource "google_compute_firewall" "allow_rtmp" {
+  name    = "permitir-rtmp-tf"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["1935"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# Regla para Web (HTTP/S para el Player y HLS)
+resource "google_compute_firewall" "allow_web" {
+  name    = "permitir-web-tf"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# Regla para SSH seguro (Google IAP)
+resource "google_compute_firewall" "allow_ssh_iap" {
+  name    = "permitir-ssh-google-iap-tf"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+}
